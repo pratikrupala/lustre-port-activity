de4add2876bb7fb510f28f31abd99bacf66e64b0
pratikrupala <prtrpl@gmail.com>
2014-10-14 22:29:03 +0530

    These files needs to be changed in order to successfully build lustre client on Linux kernel 3.16.3 The major changes are change in direct-io definition, removal of APIs like generic_file_aio_read and generic_file_aio_write in 3.16.3 kernel.

--- lustre/include/lclient.h
+++ lustre/include/lclient.h
@@ -94,16 +94,17 @@ struct ccc_io {
         /**
          * I/O vector information to or from which read/write is going.
          */
-        struct iovec *cui_iov;
-        unsigned long cui_nrsegs;
+//        struct iovec *cui_iov;
+//        unsigned long cui_nrsegs;
         /**
          * Total iov count for left IO.
          */
-        unsigned long cui_tot_nrsegs;
+//        unsigned long cui_tot_nrsegs;
         /**
          * Old length for iov that was truncated partially.
          */
-        size_t cui_iov_olen;
+//        size_t cui_iov_olen;
+	  struct iov_iter *cui_iter;
         /**
          * Total size for the left IO.
          */
--- lustre/llite/vvp_io.c
+++ lustre/llite/vvp_io.c
@@ -43,6 +43,7 @@
 
 
 #include <obd.h>
+#include <linux/uio.h>
 #include "vvp_internal.h"
 
 static struct vvp_io *cl2vvp_io(const struct lu_env *env,
@@ -231,9 +232,10 @@ static int vvp_mmap_locks(const struct lu_env *env,
         struct cl_lock_descr   *descr = &cti->cti_descr;
         ldlm_policy_data_t      policy;
         unsigned long           addr;
-        unsigned long           seg;
         ssize_t                 count;
         int                     result;
+	struct 			iov_iter i;
+	struct 			iovec iov;
         ENTRY;
 
         LASSERT(io->ci_type == CIT_READ || io->ci_type == CIT_WRITE);
@@ -241,18 +243,16 @@ static int vvp_mmap_locks(const struct lu_env *env,
         if (!cl_is_normalio(env, io))
                 RETURN(0);
 
-        if (vio->cui_iov == NULL) /* nfs or loop back device write */
+        if (vio->cui_iter == NULL) /* nfs or loop back device write */
                 RETURN(0);
 
         /* No MM (e.g. NFS)? No vmas too. */
         if (mm == NULL)
                 RETURN(0);
 
-        for (seg = 0; seg < vio->cui_nrsegs; seg++) {
-                const struct iovec *iv = &vio->cui_iov[seg];
-
-                addr = (unsigned long)iv->iov_base;
-                count = iv->iov_len;
+	iov_for_each(iov, i, *(vio->cui_iter)) {
+		addr = (unsigned long)iov.iov_base;
+		count = iov.iov_len;
                 if (count == 0)
                         continue;
 
@@ -546,9 +546,7 @@ static int vvp_io_read_start(const struct lu_env *env,
         switch (vio->cui_io_subtype) {
         case IO_NORMAL:
 		LASSERT(cio->cui_iocb->ki_pos == pos);
-		result = generic_file_aio_read(cio->cui_iocb,
-					       cio->cui_iov, cio->cui_nrsegs,
-					       cio->cui_iocb->ki_pos);
+		result = generic_file_read_iter(cio->cui_iocb, cio->cui_iter);
 		break;
         case IO_SPLICE:
                 result = generic_file_splice_read(file, &pos,
@@ -800,7 +798,7 @@ static int vvp_io_write_start(const struct lu_env *env,
 
 	CDEBUG(D_VFSTRACE, "write: [%lli, %lli)\n", pos, pos + (long long)cnt);
 
-	if (cio->cui_iov == NULL) {
+	if (cio->cui_iter == NULL) {
 		/* from a temp io in ll_cl_init(). */
 		result = 0;
 	} else {
@@ -812,9 +810,7 @@ static int vvp_io_write_start(const struct lu_env *env,
 		 * consistency, proper locking to protect against writes,
 		 * trucates, etc. is handled in the higher layers of lustre.
 		 */
-		result = __generic_file_aio_write(cio->cui_iocb,
-						  cio->cui_iov, cio->cui_nrsegs,
-						  &cio->cui_iocb->ki_pos);
+		result = generic_file_write_iter(cio->cui_iocb, cio->cui_iter);
 		if (result > 0 || result == -EIOCBQUEUED) {
 			ssize_t err;
 
@@ -1179,10 +1175,8 @@ int vvp_io_init(const struct lu_env *env, struct cl_object *obj,
                  *  results."  -- Single Unix Spec */
                 if (count == 0)
                         result = 1;
-                else {
+                else
                         cio->cui_tot_count = count;
-                        cio->cui_tot_nrsegs = 0;
-                }
 
 		/* for read/write, we store the jobid in the inode, and
 		 * it'll be fetched by osc when building RPC.
